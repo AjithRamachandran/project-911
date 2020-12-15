c7bedf188b3ffb389d6d5104fadaa17b2cab5c9c
Ajith Ramachandran <ajithar204@gmail.com>
2020-12-15 21:47:36 +0530

    renamed app to ichor

--- ichor/core/utils.py
+++ ichor/core/utils.py
@@ -1,18 +1,18 @@
 from django.contrib.auth import get_user_model
-from django.urls import reverse
+from django.urls import reverse_lazy
 
 # constants
 
-CREATE_USER_URL = reverse('users:signup')
-LOGIN_USER_URL = reverse('users:login')
-CHECK_USER_URL = reverse('users:check_user')
-GET_USER_URL = reverse('users:user_details')
-LOGOUT_USER_URL = reverse('users:logout')
-CHANGE_PWD_URL = reverse('users:change_pwd')
-CHANGE_MAIL_URL = reverse('users:change_mail')
-GET_PROFILE_URL = reverse('profiles:get_profile')
-EDIT_PROFILE_URL = reverse('profiles:profile_edit')
-GET_BLOOD_BANKS_URL = reverse('bb:blood_banks')
+CREATE_USER_URL = reverse_lazy('users:signup')
+LOGIN_USER_URL = reverse_lazy('users:login')
+CHECK_USER_URL = reverse_lazy('users:check_user')
+GET_USER_URL = reverse_lazy('users:user_details')
+LOGOUT_USER_URL = reverse_lazy('users:logout')
+CHANGE_PWD_URL = reverse_lazy('users:change_pwd')
+CHANGE_MAIL_URL = reverse_lazy('users:change_mail')
+GET_PROFILE_URL = reverse_lazy('profiles:get_profile')
+EDIT_PROFILE_URL = reverse_lazy('profiles:profile_edit')
+GET_BLOOD_BANKS_URL = reverse_lazy('bb:blood_banks')
 
 # end of constants
 
@@ -32,7 +32,7 @@ class UtilityFunctions():
         if x_forwarded_for:
             ip = x_forwarded_for.split(',')[0]
         else:
-            ip = '127.0.0.1' # check if localhost.
+            ip = '127.0.0.1'
         if ip in whitelisted_domains:
             return False
         return True
